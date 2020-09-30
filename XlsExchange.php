c5f4e1fed0ded6f1a491e471c0552ec9cf1e1de2
Kiril Osiyuk <kiril.saradomin@gmail.com>
2020-09-30 12:56:41 +0300

    validate EAN-13

---
+++ XlsExchange.php
@@ -0,0 +1,47 @@
+<?php
+
+
+trait validateEAN13 {
+
+	protected function validateEAN13(string $barcode) : ?string
+	{
+		$code = intval(substr($barcode, 0, 3));
+		$original_sum = intval($barcode[12]);
+
+		if (strlen($barcode) != 13) {
+			return null;
+		}
+
+		if ($code < 200 || 299 < $code) {
+			return null;
+		}
+
+		for ($sum = $i = 0; $i < 12; $i++) {
+			$num = intval($barcode[$i]);
+			$sum += ($num % 2 ? 3 : 1) * $num;
+		}
+
+		$check_sum = (10 - $sum % 10) % 10;
+
+		if ($original_sum !== $check_sum) {
+			return null;
+		}
+
+		return $barcode;
+	}
+}
+
+
+final class XlsExchange {
+	use validateEAN13;
+
+	private const INVALID_BCODE = 'INVALID_BCODE';
+
+	public function testEAN13(string $barcode)
+	{
+		$result = $this->validateEAN13($barcode) ?? self::INVALID_BCODE;
+		echo "$barcode validated $result\n";
+		return $this;
+	}
+}
+
---
+++ XlsExchangeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'XlsExchange.php';
+
+(new XlsExchange())
+	->testEAN13('2109970000015')
+	->testEAN13('2154231000019')
+	->testEAN13('2109991000018')
+	->testEAN13('2151212000017')
+	->testEAN13('2109980000012')
+	->testEAN13('2151246000014')
+	->testEAN13('2151246000019')
+	;
+
